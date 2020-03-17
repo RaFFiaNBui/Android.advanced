5f08d19489f3bbbdb2e8c3e304c2b0049040b2d8
RaFFiaNBui <Raffian_bui@mail.ru>
2020-03-17 22:34:29 +0300

    теперь отображаем начальный фрагмент программно

--- DRAFT/app/src/main/java/com/example/ablesson_1/MainActivity.java
+++ DRAFT/app/src/main/java/com/example/ablesson_1/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.ablesson_1;
 
 import android.os.Bundle;
 
+import androidx.fragment.app.FragmentTransaction;
+
 public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         initMenu();
+
+        if (savedInstanceState == null) {
+            MainFragment mainFragment = new MainFragment();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(R.id.first_fragment,mainFragment);
+            ft.commit();
+        }
     }
 }
