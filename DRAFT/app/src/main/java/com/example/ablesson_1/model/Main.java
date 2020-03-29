d8909ba311b7e3d18a499a2a4ca84f3719ab57df
RaFFiaNBui <Raffian_bui@mail.ru>
2020-03-29 21:08:02 +0300

    ДЗ 4 Доступ в сеть теперь через Retrifit добавлены аннотации

--- DRAFT/app/src/main/java/com/example/ablesson_1/model/Main.java
+++ DRAFT/app/src/main/java/com/example/ablesson_1/model/Main.java
@@ -1,8 +1,18 @@
 package com.example.ablesson_1.model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class Main {
+
+    @SerializedName("temp")
+    @Expose
     private float temp;
+    @SerializedName("pressure")
+    @Expose
     private int pressure;
+    @SerializedName("humidity")
+    @Expose
     private int humidity;
 
     public int getTemp() {
