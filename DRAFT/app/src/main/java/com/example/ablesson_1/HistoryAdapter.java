a66d889d6b905f0f1013f8e57d1b5b4d3b538a15
RaFFiaNBui <Raffian_bui@mail.ru>
2020-04-09 14:41:27 +0300

    ДЗ 5 реализация Room закомментирован старый код

--- DRAFT/app/src/main/java/com/example/ablesson_1/HistoryAdapter.java
+++ DRAFT/app/src/main/java/com/example/ablesson_1/HistoryAdapter.java
@@ -8,16 +8,26 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
+import com.example.ablesson_1.history.HistorySource;
+import com.example.ablesson_1.history.LineOfHistory;
 
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Locale;
+
+//реализация с ArrayList неиспользуется после подключения Room
 public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.HistoryHolder> {
 
-    private ArrayList<String> dataCity;
-    private ArrayList<String> dataTemp;
+    //private ArrayList<String> dataCity;
+    //private ArrayList<String> dataTemp;
 
-    HistoryAdapter(ArrayList<String> dataSity, ArrayList<String> dataTemp) {
-        this.dataCity = dataSity;
-        this.dataTemp = dataTemp;
+    private HistorySource dataSource;
+
+    HistoryAdapter(/*ArrayList<String> dataSity, ArrayList<String> dataTemp*/
+            HistorySource dataSource) {
+        /*this.dataCity = dataSity;
+        this.dataTemp = dataTemp;*/
+        this.dataSource = dataSource;
     }
 
     @NonNull
@@ -29,28 +39,39 @@ public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.HistoryH
 
     @Override
     public void onBindViewHolder(@NonNull HistoryHolder holder, int position) {
-        holder.setData(dataCity.get(position), dataTemp.get(position));
+        //holder.setData(dataCity.get(position), dataTemp.get(position));
+        List<LineOfHistory> linies = dataSource.getLinies();
+        LineOfHistory lineOfHistory = linies.get(position);
+        holder.setData(lineOfHistory.cityName, lineOfHistory.cityTemp, lineOfHistory.date);
     }
 
     @Override
-    public int getItemCount() {
+    /*public int getItemCount() {
         return dataCity.size();
+    }*/
+    public int getItemCount() {
+        return (int) dataSource.getCountLinies();
     }
 
     class HistoryHolder extends RecyclerView.ViewHolder {
 
         private TextView textViewCity;
         private TextView textViewTemp;
+        private TextView textViewDate;
 
         HistoryHolder(@NonNull View itemView) {
             super(itemView);
             textViewCity = itemView.findViewById(R.id.item_history_city);
             textViewTemp = itemView.findViewById(R.id.item_history_temp);
+            textViewDate = itemView.findViewById(R.id.item_history_date);
         }
 
-        void setData(final String city, final String temp) {
+        void setData(final String city, final String temp, long date) {
             textViewCity.setText(city);
             textViewTemp.setText(temp);
+            SimpleDateFormat smp = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault());
+            String str = String.format(Locale.getDefault(), "%s", smp.format(date * 1000L));
+            textViewDate.setText(str);
         }
     }
 }
