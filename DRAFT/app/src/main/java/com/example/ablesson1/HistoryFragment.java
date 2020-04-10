d0b6688f5c00ebbcbfae00832b09c001d010bb76
RaFFiaNBui <Raffian_bui@mail.ru>
2020-04-10 11:49:01 +0300

    ДЗ 5 + контекстное меню

--- DRAFT/app/src/main/java/com/example/ablesson1/HistoryFragment.java
+++ DRAFT/app/src/main/java/com/example/ablesson1/HistoryFragment.java
@@ -1,9 +1,13 @@
 package com.example.ablesson1;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,8 +18,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.ablesson1.history.App;
 import com.example.ablesson1.history.HistoryDao;
 import com.example.ablesson1.history.HistorySource;
+import com.example.ablesson1.history.LineOfHistory;
+
+import java.util.Objects;
 
 public class HistoryFragment extends Fragment {
+
+    private HistoryAdapter historyAdapter;
+    private HistorySource historySource;
 
     @Nullable
     @Override
@@ -46,10 +56,49 @@ public class HistoryFragment extends Fragment {
         }
         //инициализируем средства для работы с БД Room
         HistoryDao historyDao = App.getInstance().getHistoryDao();
-        HistorySource historySource = new HistorySource(historyDao);
+        historySource = new HistorySource(historyDao);
         //инициализируем адаптер
-        HistoryAdapter historyAdapter = new HistoryAdapter(historySource);
+        historyAdapter = new HistoryAdapter(historySource, getActivity());
         //устанавливаем нашему списку адаптер
         recyclerView.setAdapter(historyAdapter);
+    }
+
+    @Override
+    public void onCreateContextMenu(@NonNull ContextMenu menu, @NonNull View v, @Nullable ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = Objects.requireNonNull(getActivity()).getMenuInflater();
+        inflater.inflate(R.menu.context_menu_history, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.history_filter_by_city:
+                historySource.getHistoryByName("Кострома");
+                historyAdapter.notifyDataSetChanged();
+                Toast.makeText(getContext(), "111111111111", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.history_filter_by_date:
+                historySource.getHistoryByDate(1586390400);
+                historyAdapter.notifyDataSetChanged();
+                Toast.makeText(getContext(), "22222222222222", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.history_filter_by_temp:
+                historySource.getHistoryByTemperature("14");
+                historyAdapter.notifyDataSetChanged();
+                Toast.makeText(getContext(), "333333333333", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.history_delete:
+                LineOfHistory lineOfHistory = historySource.getLines().get((int) historyAdapter.getMenuPosition());
+                historySource.deleteLine(lineOfHistory);
+                historyAdapter.notifyItemRemoved((int) historyAdapter.getMenuPosition());
+                Toast.makeText(getContext(), "44444444444444", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.history_delete_all:
+                Toast.makeText(getContext(), "555555555555", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onContextItemSelected(item);
     }
 }
