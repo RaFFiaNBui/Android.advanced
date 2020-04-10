eb810296c76f5c716347c5dceff406f5352a148d
RaFFiaNBui <Raffian_bui@mail.ru>
2020-04-10 11:46:04 +0300

    ДЗ 5 + контекстное меню (onCreateContextMenu)

--- DRAFT/app/src/main/java/com/example/ablesson1/MainActivity.java
+++ DRAFT/app/src/main/java/com/example/ablesson1/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.ablesson1;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends BaseActivity {
@@ -20,5 +25,12 @@ public class MainActivity extends BaseActivity {
             ft.add(R.id.first_fragment, cityFragment);
             ft.commit();
         }
+    }
+
+    @Override
+    public void onCreateContextMenu(@NonNull ContextMenu menu, @NonNull View v, @Nullable ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.context_menu_history, menu);
     }
 }
