ac87fbaaa9dd47c7f455e97531686efd4867f6c2
RaFFiaNBui <Raffian_bui@mail.ru>
2020-04-10 21:13:48 +0300

    ДЗ 5 onCreateContextMenu перенесен во фрагмент

--- DRAFT/app/src/main/java/com/example/ablesson1/MainActivity.java
+++ DRAFT/app/src/main/java/com/example/ablesson1/MainActivity.java
@@ -1,12 +1,7 @@
 package com.example.ablesson1;
 
 import android.os.Bundle;
-import android.view.ContextMenu;
-import android.view.MenuInflater;
-import android.view.View;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends BaseActivity {
@@ -25,12 +20,5 @@ public class MainActivity extends BaseActivity {
             ft.add(R.id.first_fragment, cityFragment);
             ft.commit();
         }
-    }
-
-    @Override
-    public void onCreateContextMenu(@NonNull ContextMenu menu, @NonNull View v, @Nullable ContextMenu.ContextMenuInfo menuInfo) {
-        super.onCreateContextMenu(menu, v, menuInfo);
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.context_menu_history, menu);
     }
 }
