021d2306e6ec6ae147f11756a422972bdcffd64b
RaFFiaNBui <Raffian_bui@mail.ru>
2020-04-10 20:00:26 +0300

    ДЗ 5 + контекстное меню + фильтры

--- DRAFT/app/src/main/java/com/example/ablesson1/history/HistorySource.java
+++ DRAFT/app/src/main/java/com/example/ablesson1/history/HistorySource.java
@@ -16,22 +16,18 @@ public class HistorySource {
 
     // Получить все строчки
     public List<LineOfHistory> getLines() {
-        // Если объекты еще не загружены, загружаем их.
-        // Это сделано для того, чтобы не делать запросы к БД каждый раз
-        if (lines == null) {
-            LoadLines();
-        }
         return lines;
     }
 
     //Загружаем все строчки в буфер
-    private void LoadLines() {
+    public void loadLines() {
         lines = historyDao.getAllLinesOfHistory();
     }
 
     //Получаем количество записей
-    public long getCountLines() {
-        return historyDao.getCountLinesOfHistory();
+    public int getCountLines() {
+        //return historyDao.getCountLinesOfHistory(); // подтягивал все записи из таблицы(для фильтра не подходит)
+        return lines.size();
     }
 
     //Добавляем строчку
@@ -41,21 +37,28 @@ public class HistorySource {
 
     //Удаляем строчку из базы
     public void deleteLine(LineOfHistory lineOfHistory) {
-        historyDao.deleteLineOfHistory(lineOfHistory);
+        historyDao.deleteLineOfHistory(lineOfHistory); //из БД
+        lines.remove(lineOfHistory);    //из текущего List
+    }
+
+    //Очистить БД
+    public void deleteAll() {
+        historyDao.deleteAll(); //из БД
+        lines.clear();  //из List
     }
 
     //Получим данные только об одном городе
-    public void getHistoryByName (String name) {
-        historyDao.getHistoryByName(name);
+    public void getHistoryByName(String name) {
+        lines = historyDao.getHistoryByName(name);
     }
 
     // Получим данные только по дате
-    public void getHistoryByDate (long date) {
-        historyDao.getHistoryByDate(date);
+    public void getHistoryByDate(long date) {
+        lines = historyDao.getHistoryByDate(date);
     }
 
     // Получим данные только по температуре
-    public void getHistoryByTemperature (String temp) {
-        historyDao.getHistoryByTemperature(temp);
+    public void getHistoryByTemperature(String temp) {
+        lines = historyDao.getHistoryByTemperature(temp);
     }
 }
