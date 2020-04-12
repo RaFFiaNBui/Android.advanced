d97cc4081f30317c911a822609660954cbd6cd46
RaFFiaNBui <Raffian_bui@mail.ru>
2020-04-12 16:18:06 +0300

    ДЗ 6 + broadcastReceiver на низкий заряд батареи

--- DRAFT/app/src/main/java/com/example/ablesson1/MainActivity.java
+++ DRAFT/app/src/main/java/com/example/ablesson1/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.ablesson1;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends BaseActivity {
+
+    MyBroadcastReceiver receiver = new MyBroadcastReceiver();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,13 +20,33 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
 
         initMenu();
-
         //вывод на начальный экран главного фрагмента
+        showFirstScreen(savedInstanceState);
+        //инициализация канала нотификаций
+        initNotificationChannel();
+        //програмная регистрация ресивера о низком заряде батареи
+        registerReceiver(receiver, new IntentFilter(Intent.ACTION_BATTERY_LOW));
+    }
+
+    //вывод на начальный экран главного фрагмента
+    private void showFirstScreen(Bundle savedInstanceState) {
         if (savedInstanceState == null) {
             CityFragment cityFragment = new CityFragment();
             FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
             ft.add(R.id.first_fragment, cityFragment);
             ft.commit();
+        }
+    }
+
+    //инициализация канала нотификаций
+    private void initNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            int importance = NotificationManager.IMPORTANCE_LOW;
+            NotificationChannel channel = new NotificationChannel("2", "myChannel", importance);
+            if (notificationManager != null) {
+                notificationManager.createNotificationChannel(channel);
+            }
         }
     }
 }
