91b8875fa43dcc73ff8a73983faf67b48e1cf586
RaFFiaNBui <Raffian_bui@mail.ru>
2020-04-13 14:46:05 +0300

    ДЗ 6 + push уведомления через Firebase - Cloud Messaging

--- DRAFT/app/src/main/java/com/example/ablesson1/MainActivity.java
+++ DRAFT/app/src/main/java/com/example/ablesson1/MainActivity.java
@@ -5,10 +5,19 @@ import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 public class MainActivity extends BaseActivity {
 
@@ -26,6 +35,8 @@ public class MainActivity extends BaseActivity {
         initNotificationChannel();
         //програмная регистрация ресивера о низком заряде батареи
         registerReceiver(receiver, new IntentFilter(Intent.ACTION_BATTERY_LOW));
+        registerReceiver(receiver, new IntentFilter(ConnectivityManager.EXTRA_NO_CONNECTIVITY));
+        initGetToken();
     }
 
     //вывод на начальный экран главного фрагмента
@@ -48,5 +59,24 @@ public class MainActivity extends BaseActivity {
                 notificationManager.createNotificationChannel(channel);
             }
         }
+    }
+
+    //получение токена
+    private void initGetToken() {
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w("MyLog", "getInstanceId failed", task.getException());
+                            return;
+                        }
+                        // получим наш token
+                        String token = task.getResult().getToken();
+                        // token to Log and toast
+                        Log.d("MyLog", "My Token:" + token);
+                        Toast.makeText(MainActivity.this, "Token received", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
