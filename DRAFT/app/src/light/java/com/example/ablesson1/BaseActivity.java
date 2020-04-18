da2ce86198483f4e2f72b34d68bf78bc9a8cc460
RaFFiaNBui <Raffian_bui@mail.ru>
2020-04-18 12:49:31 +0300

    ДЗ 8 + flavors: light - без location и google map full - c location и google map
    - без локации

---
+++ DRAFT/app/src/light/java/com/example/ablesson1/BaseActivity.java
@@ -0,0 +1,192 @@
+package com.example.ablesson1;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TableRow;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+
+public class BaseActivity extends AppCompatActivity implements Constants, NavigationView.OnNavigationItemSelectedListener {
+
+    private static final int SETTINGS_CODE = 555;
+    private boolean currentThemeIsDark; //true - текущая тема ТЕМНАЯ
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        //установим тему
+        if (isDarkTheme()) {
+            setTheme(R.style.DarkTheme);
+            currentThemeIsDark = true;
+        } else {
+            setTheme(R.style.AppTheme);
+            currentThemeIsDark = false;
+        }
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        // проверяем не изменилась ли наша тема
+        if (isDarkTheme() != currentThemeIsDark) {
+            recreate();
+        }
+        super.onStart();
+    }
+
+    // Чтение настроек, параметр тема
+    protected boolean isDarkTheme() {
+        // Работаем через специальный класс сохранения и чтения настроек
+        SharedPreferences sharedPref = getSharedPreferences(SHARED_PREFERENCE_KEY, MODE_PRIVATE);
+        //Прочитать тему, если настройка не найдена - взять по умолчанию false
+        return sharedPref.getBoolean(IS_DARK_THEME, false);
+    }
+
+    // Сохранение настроек
+    protected void setDarkTheme(boolean isDarkTheme) {
+        SharedPreferences sharedPref = getSharedPreferences(SHARED_PREFERENCE_KEY, MODE_PRIVATE);
+        // Настройки сохраняются посредством специального класса editor.
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putBoolean(IS_DARK_THEME, isDarkTheme);
+        editor.apply();
+    }
+
+    //создаем меню
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        //инициализация пункта меню - Поиск
+        MenuItem search = menu.findItem(R.id.search_bar);
+        //строка поиска
+        final SearchView searchText = (SearchView) search.getActionView();
+        searchText.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            //реакция на окончание ввода поиска
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Snackbar.make(searchText, query, Snackbar.LENGTH_LONG).show();
+                return true;
+            }
+
+            //Реагирует на нажатие каждой клавиши
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return true;
+            }
+        });
+        return true;
+    }
+
+    //привязываем активити настроек к меню
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivityForResult(intent, SETTINGS_CODE);
+                return true;
+            case R.id.about_developers:
+                Intent intentDev = new Intent(this, DevelopersActivity.class);
+                startActivity(intentDev);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //отображение настроек
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode != SETTINGS_CODE) {
+            super.onActivityResult(requestCode, resultCode, data);
+            return;
+        }
+        if (resultCode == RESULT_OK) {
+            changeSettings();
+            recreate();
+        }
+    }
+
+    private void changeSettings() {
+        SharedPreferences sharedPref = getSharedPreferences(SHARED_PREFERENCE_KEY, MODE_PRIVATE);
+        //отрисовка восхода и заката
+        boolean tempSun = sharedPref.getBoolean(SUN, true);
+        TableRow TRSunset = findViewById(R.id.sunset_row);
+        if (TRSunset != null) {
+            TableRow TRSunrise = findViewById(R.id.sunrise_row);
+            TRSunset.setVisibility(tempSun ? View.VISIBLE : View.GONE);
+            TRSunrise.setVisibility(tempSun ? View.VISIBLE : View.GONE);
+            //отрисовка давления
+            boolean tempPressure = sharedPref.getBoolean(PRESSURE, true);
+            TableRow TRPressure = findViewById(R.id.pressure_row);
+            TRPressure.setVisibility(tempPressure ? View.VISIBLE : View.GONE);
+            //отрисовка ветра
+            boolean tempWind = sharedPref.getBoolean(WIND, true);
+            TableRow TRWind = findViewById(R.id.wind_row);
+            TRWind.setVisibility(tempWind ? View.VISIBLE : View.GONE);
+        }
+    }
+
+    void initMenu() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        switch (item.getItemId()) {
+            case (R.id.nav_start):
+                ft.replace(R.id.first_fragment, new MainFragment());
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                ft.addToBackStack("Start");
+                ft.commit();
+                break;
+            case (R.id.nav_settings):
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivityForResult(intent, SETTINGS_CODE);
+                break;
+            case (R.id.nav_history):
+                ft.replace(R.id.first_fragment, new HistoryFragment());
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                ft.addToBackStack("Start");
+                ft.commit();
+                break;
+            case (R.id.nav_developers):
+                Intent intentDev = new Intent(this, DevelopersActivity.class);
+                startActivity(intentDev);
+                break;
+        }
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer != null && drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
---
+++ DRAFT/app/src/light/java/com/example/ablesson1/CityFragment.java
@@ -0,0 +1,300 @@
+package com.example.ablesson1;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ScrollView;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.ablesson1.history.App;
+import com.example.ablesson1.history.HistoryDao;
+import com.example.ablesson1.history.HistorySource;
+import com.example.ablesson1.history.LineOfHistory;
+import com.example.ablesson1.model.WeatherRequest;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Objects;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static android.content.Context.MODE_PRIVATE;
+
+
+public class CityFragment extends Fragment implements Constants {
+
+    private static final String WEATHER_API_KEY = "14f34cd242746f2d76bb04739d7485fe"; //временный Api
+    private String currentCity;  //будет привязана геолокация
+
+    private TextView currentName;
+    private TextView currentTemperature;
+    private TextView currentHumidity;
+    private TextView sunrise;
+    private TextView sunset;
+    private TextView currentPressure;
+    private TextView windSpeed;
+
+    private WeatherRequestByCity weatherRequestByCity;
+
+    // Фабричный метод создания фрагмента
+    // Фрагменты рекомендуется создавать через фабричные методы.
+    static CityFragment create(Parcel parcel) {
+        CityFragment f = new CityFragment();    // создание
+
+        // Передача параметра
+        Bundle args = new Bundle();
+        args.putSerializable(PARCEL, parcel);
+        f.setArguments(args);
+        return f;
+    }
+
+    // Получить посылку из параметра
+    Parcel getParcel() {
+        Parcel parcel = null;
+        if (getArguments() != null) {
+            parcel = (Parcel) getArguments().getSerializable(PARCEL);
+        }
+        return parcel;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Определить какой герб надо показать, и показать его
+        View layout = inflater.inflate(R.layout.fragment_city, container, false);
+        TextView textViewCity = layout.findViewById(R.id.city);
+        changeSettings(layout);
+        Parcel parcel = getParcel();
+        //Если это 1 запуск, то берем настройки из SharedPreference, если нет - из Parcel
+        //В дальнейшем Parcel можно убрать вообще и использовать только SharedPreference
+        if (parcel == null) {
+            SharedPreferences sPref = Objects.requireNonNull(getActivity()).getSharedPreferences(SHARED_PREFERENCE_KEY, MODE_PRIVATE);
+            currentCity = sPref.getString(CITY, "Москва");
+        } else {
+            currentCity = parcel.getCityName();
+        }
+        textViewCity.setText(currentCity);
+        return layout;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //подтягиваем наш список городов
+        String[] data = getResources().getStringArray(R.array.items_week_array);
+        //инициализируем RecyclerView
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view_week);
+        //подсказываем, что наш список конечный
+        recyclerView.setHasFixedSize(true);
+        //инициализируем LayoutManager (повторно, он был проинициализирован в fragment_main)
+        if (getContext() != null) {
+            recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.HORIZONTAL, false));
+        }
+        //инициализируем адаптер
+        WeekAdapter weekAdapter = new WeekAdapter(data);
+        //устанавливаем нашему списку адаптер
+        recyclerView.setAdapter(weekAdapter);
+        //инициализация фонового изображения
+        ScrollView scrollView = view.findViewById(R.id.scrollView);
+        SharedPreferences sharedPref = Objects.requireNonNull(this.getActivity()).getSharedPreferences(SHARED_PREFERENCE_KEY, MODE_PRIVATE);
+        if (sharedPref.getBoolean(IS_DARK_THEME, false)) {
+            scrollView.setBackgroundResource(R.drawable.sky_night);
+        } else {
+            scrollView.setBackgroundResource(R.drawable.sky_day);
+        }
+        //отображение текущей даты
+        setDate(view);
+        //инициализация текстовых полей
+        init(view);
+        //устанавливаем соединение версия 2 использовалась с классом Connection и DataParsing
+        //new Connection(currentCity, onDataLoadedListener, exceptionListener);
+        // установка параметров для запроса
+        String lang;    //выбор языка
+        String units;   //выбор системы измерений
+        if (Locale.getDefault().getLanguage().equals("ru")) {
+            lang = "ru";
+            units = "metric";
+        } else {
+            lang = "en";
+            units = "imperial";
+        }
+        //проверка сети
+        checkConnection();
+        //установка соединения по средствам Retrofit
+        initRetrofit();
+        requestRetrofit(currentCity, units, lang);
+    }
+
+    //метод отрисовки необходимых настроек
+    private void changeSettings(View v) {
+        SharedPreferences sharedPref = Objects.requireNonNull(getActivity()).getSharedPreferences(SHARED_PREFERENCE_KEY, MODE_PRIVATE);
+        //отрисовка восхода и заката
+        boolean tempSun = sharedPref.getBoolean(SUN, true);
+        TableRow TRSunset = v.findViewById(R.id.sunset_row);
+        if (TRSunset != null) {
+            TableRow TRSunrise = v.findViewById(R.id.sunrise_row);
+            TRSunset.setVisibility(tempSun ? View.VISIBLE : View.GONE);
+            TRSunrise.setVisibility(tempSun ? View.VISIBLE : View.GONE);
+            //отрисовка давления
+            boolean tempPressure = sharedPref.getBoolean(PRESSURE, true);
+            TableRow TRPressure = v.findViewById(R.id.pressure_row);
+            TRPressure.setVisibility(tempPressure ? View.VISIBLE : View.GONE);
+            //отрисовка ветра
+            boolean tempWind = sharedPref.getBoolean(WIND, true);
+            TableRow TRWind = v.findViewById(R.id.wind_row);
+            TRWind.setVisibility(tempWind ? View.VISIBLE : View.GONE);
+        }
+    }
+
+    //метод отображения текущей даты
+    private void setDate(View view) {
+        Date currentDate = new Date();
+        TextView textViewCurrentDate = view.findViewById(R.id.current_date);
+        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault());
+        textViewCurrentDate.setText(dateFormat.format(currentDate));
+
+        //установка дня недели
+        TextView textViewDayOfWeek = view.findViewById(R.id.current_day_of_week);
+        DateFormat dayOfWeekFormat = new SimpleDateFormat("EEEE", Locale.getDefault());
+        textViewDayOfWeek.setText(dayOfWeekFormat.format(currentDate));
+
+        //установка времени
+        TextView textViewCurrentTime = view.findViewById(R.id.current_time);
+        DateFormat timeFormat = new SimpleDateFormat("HH:mm", Locale.getDefault());
+        textViewCurrentTime.setText(timeFormat.format(currentDate));
+    }
+
+    //инициализация полей
+    private void init(View view) {
+        currentTemperature = view.findViewById(R.id.t_day);
+        currentHumidity = view.findViewById(R.id.humidity);
+        sunrise = view.findViewById(R.id.sunrise);
+        sunset = view.findViewById(R.id.sunset);
+        currentPressure = view.findViewById(R.id.pressure);
+        windSpeed = view.findViewById(R.id.wind);
+        currentName = view.findViewById(R.id.city);
+    }
+
+    //проверка доступности сети
+    private void checkConnection() {
+        ConnectivityManager connManager = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connManager.getActiveNetworkInfo();
+        Intent intent = new Intent(NETWORK_IS_CONNECTED);
+        if (!Objects.requireNonNull(networkInfo).isConnected()) {
+            getActivity().sendBroadcast(intent);
+        }
+    }
+
+    private void initRetrofit() {
+        Retrofit retrofit;
+        retrofit = new Retrofit.Builder()
+                .baseUrl("http://api.openweathermap.org/")  //базовая часть адреса
+                .addConverterFactory(GsonConverterFactory.create()) //конвертер для преобразования из json в объкект
+                .client(getLoggingOkHttpClient())
+                .build();
+        weatherRequestByCity = retrofit.create(WeatherRequestByCity.class);   //создаем объект при помощи которогобудем выполнять запросы
+    }
+
+    private OkHttpClient getLoggingOkHttpClient() {
+        //Сначала создаем HttpLoggingInterceptor. В нем настраиваем уровень логирования.
+        // Если у нас Debug билд, то выставляем максимальный уровень (BODY), иначе - ничего
+        // не логируем, чтобы не палить в логах релизные запросы.
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(BuildConfig.DEBUG ? HttpLoggingInterceptor.Level.BODY : HttpLoggingInterceptor.Level.NONE);
+
+        //HttpLoggingInterceptor мы не можем напрямую передать в Retrofit билдер. Поэтому
+        // сначала создаем OkHttpClient, ему передаем HttpLoggingInterceptor, и уже этот
+        // OkHttpClient используем в Retrofit билдере
+        return new OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .build();
+    }
+
+    private void requestRetrofit(String city, String units, String lang) {
+        weatherRequestByCity.loadWeather(city, units, lang, CityFragment.WEATHER_API_KEY)
+                .enqueue(getCallback());
+    }
+
+    private Callback<WeatherRequest> getCallback() {
+        return new Callback<WeatherRequest>() {
+            @Override
+            public void onResponse(Call<WeatherRequest> call, Response<WeatherRequest> response) {
+                if (response.body() != null) {
+                    String temp = String.format(Locale.getDefault(), "%d", response.body().getMain().getTemp());
+                    currentTemperature.setText(temp);
+                    currentHumidity.setText(String.format(Locale.getDefault(), "%d", response.body().getMain().getHumidity()));
+                    currentPressure.setText(String.format(Locale.getDefault(), "%d", response.body().getMain().getPressure()));
+                    windSpeed.setText(String.format(Locale.getDefault(), "%d", response.body().getWind().getSpeed()));
+                    currentCity = String.format(Locale.getDefault(), "%s", response.body().getName());
+                    currentName.setText(currentCity);
+                    //Время восхода и заката приводим к привычному виду
+                    SimpleDateFormat smp = new SimpleDateFormat("HH:mm", Locale.getDefault());
+                    sunrise.setText(String.format(Locale.getDefault(), "%s", smp.format(response.body().getSys().getSunrise() * 1000L)));
+                    sunset.setText(String.format(Locale.getDefault(), "%s", smp.format(response.body().getSys().getSunset() * 1000L)));
+                    //saveHistory(temp);    //неиспользуется после подключения Room
+                    saveHistoryRoom(temp);  //сохранение истории в БД Room
+                    saveCity(); //сохранение текущего города в SharedPreference
+                } else {
+                    Log.e("MyLog", "onResponse: Город не был найден на сервере code=" + response.code() + " message=" + response.message());
+                    String message = getResources().getString(R.string.error_msg_part_1) + currentCity + getResources().getString(R.string.error_msg_part_2);
+                    //создаем диалоговое окно с необходимым нам сообщением
+                    if (getFragmentManager() != null) {
+                        MyDialogFragment.create(message).show(getFragmentManager(), "Exception");
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<WeatherRequest> call, Throwable t) {
+                Log.e("MyLog", "onFailure: Ошибка соединения", t);  //код в случае ошибки соединения
+                String message = getResources().getString(R.string.fail_connection);
+                //создаем диалоговое окно с необходимым нам сообщением
+                if (getFragmentManager() != null) {
+                    MyDialogFragment.create(message).show(getFragmentManager(), "Exception");
+                }
+            }
+        };
+    }
+
+    //сохранение истории в БД Room
+    private void saveHistoryRoom(String temp) {
+        HistoryDao historyDao = App.getInstance().getHistoryDao();
+        HistorySource historySource = new HistorySource(historyDao);
+        LineOfHistory lineOfHistory = new LineOfHistory();
+        lineOfHistory.cityName = currentCity;
+        lineOfHistory.cityTemp = temp;
+        lineOfHistory.date = (System.currentTimeMillis() / 1000);
+        historySource.addLine(lineOfHistory);
+    }
+
+    //сохранение текущего города в SharedPreference
+    private void saveCity() {
+        SharedPreferences preferences = Objects.requireNonNull(getActivity()).getSharedPreferences(SHARED_PREFERENCE_KEY, MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(CITY, currentCity);
+        editor.apply();
+    }
+}
